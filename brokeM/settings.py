b8b4773bb8ca1eb6a24e0fa3e588efd5a10ee0c4
BrokMakeT <brokmaket3@gmail.com>
2025-02-10 18:56:11 -0600

    Update settings.py

--- brokeM/settings.py
+++ brokeM/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'RENDER' not in os.environ
 
 ALLOWED_HOSTS = ["*"]
 
